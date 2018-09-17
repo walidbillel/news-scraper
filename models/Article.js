5bd64e981d9abbad0a0ac38cc54029386dd8d5dd
willou <walid.boubguira@gmail.com>
2018-09-17 18:52:10 -0400

    data

--- models/Article.js
+++ models/Article.js
@@ -6,28 +6,28 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
-  title: {
-    type: String,
-    required: true
-  },
+    // `title` is required and of type String
+    title: {
+        type: String,
+        required: true
+    },
     //   Summary of the article
-  summary: {
-      type: String,
-      required: true
-  },
-  // `link` is required and of type String
-  link: {
-    type: String,
-    required: true
-  },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  notes: {
-    type: Schema.Types.ObjectId,
-    ref: "Notes"
-  }
+    summary: {
+        type: String,
+        required: true
+    },
+    // `link` is required and of type String
+    link: {
+        type: String,
+        required: true
+    },
+    // `note` is an object that stores a Note id
+    // The ref property links the ObjectId to the Note model
+    // This allows us to populate the Article with an associated Note
+    notes: {
+        type: Schema.Types.ObjectId,
+        ref: "Notes"
+    }
 });
 
 // This creates our model from the above schema, using mongoose's model method
--- server.js
+++ server.js
@@ -2,23 +2,142 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
-var hbs = require('express-handlebars');
-var path = require("path");
+var logger = require("morgan");
+// var hbs = require('express-handlebars');
+// var path = require("path");
+// var axios = require("axios");
+var request = require("request");
 var PORT = 3000;
 
-// var db = require("./models");s
+var db = require("./models");
+
 
 var app = express();
 
-var routes = "./routes";
+app.use(express.static("public"));
 
-// app.use(routes);
-mongoose.connect("mongodb://localhost/insider_db", { useNewUrlParser: true });
-
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, './views'));
-app.engine('hbs', hbs({extname: '.hbs', defaultLayout: 'main'}))
-app.set('view engine', 'hbs');
+
+
+
+
+mongoose.connect("mongodb://localhost/New_York_Times_Tec", { useNewUrlParser: true });
+
+// Scrape 
+app.get("/scrape", function (req, res) {
+
+    request("https://www.nytimes.com/section/technology", function (err, response, html) {
+        // console.log("yoooooo", a .title);
+        var $ = cheerio.load(html);
+
+        // res.send(html);
+
+        $("ol.story-menu.theme-stream.initial-set").children("li").each(function (i, element) {
+
+            var articles = {};
+            var headlineMess = $(element).children(".story").children(".story-body").children(".story-link").children(".story-meta").children(".headline").text();
+            articles.title = headlineMess.split("\n").map(function (a) { return a.trim() })[1]
+            articles.summary = $(element).children(".story").children(".story-body").children(".story-link").children(".story-meta").children(".summary").text();
+            articles.link = $(element).children(".story").children(".story-body").children(".story-link").attr("href");
+            // console.log(story)
+
+            // var headline = $("h2.headline").text();
+            // var body = $("p.summary").text();
+            db.Article.create(articles, function (err) {
+                if (err) {
+                    res.status("500");
+                }
+            });
+            console.log(articles);
+
+            // console.log(body);
+
+
+            // console.log(story);
+        });
+
+        //  console.log($);
+        // res.render(html);
+    });
+    
+});
+
+app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id } }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+app.delete("/notes/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.deleteOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .then(function (dbNote) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbNote);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+app.get("/notes", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Note.find({})
+        .then(function (dbNote) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbNote);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
