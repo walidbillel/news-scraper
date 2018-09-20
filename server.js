08e7a8bb385d674b8f6b3c22a28453a969ddbf0c
willou <walid.boubguira@gmail.com>
2018-09-20 09:32:54 -0400

    note fixed

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
-        .populate("notes")
+        .populate("note")
         .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
